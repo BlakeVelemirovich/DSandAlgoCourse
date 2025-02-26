9150a2eb3d48cffa2c069aa998e15bcdbd28f785
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-02-26 16:04:17 -0400

    Explaining the choice between a deque and a queue

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <deque>    // Include the deque library
-#include <string>   // Include the string library for input handling
+#include <deque>
+#include <string>
 using namespace std;
 
-// Function to check if a string is a palindrome
+/*
+I decided to use a deque over a queue because it's just a lot more natural and makes way more sense to use a 
+deque in this situation. Think about how a palindrome works, it needs to read the same way from both ends,
+so we must compare each sides of the word together and see if they are the same. If they are the same, then
+we can remove those letters, and then do a comparison from each side of the word of the remaining letters, until
+it is completed. A deque allows us to add letters to it, and then compare from each side of the deque, and then
+remove values from either side. This fits our use case perfectly. A queue on the other hand would not be as good
+because we are only able to add a letter to the back so that it pops out at the front. 
+
+It's just more intuitive to use the deque in this situation as it matches perfectly with our usecase.
+*/
+
 bool isPalindrome(const string& str) {
     // Create a deque to store characters of the string
     deque<char> charDeque;
@@ -14,6 +25,7 @@ bool isPalindrome(const string& str) {
     }
 
     // Compare characters from the front and back of the deque
+    // If they are not the same then obviously it's not a palindrome
     while (charDeque.size() > 1) {
         // Get the front character
         char frontChar = charDeque.front();
