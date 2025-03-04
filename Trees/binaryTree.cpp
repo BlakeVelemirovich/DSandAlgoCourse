726e13f2180a87855f65e9fc84f2b345c83c2e76
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-04 13:00:51 -0400

    Binary Tree Node Skeleton

---
+++ Trees/binaryTree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+int main() {
+    
+
+    return 0;
+}
