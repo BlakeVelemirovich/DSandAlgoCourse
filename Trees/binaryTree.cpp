3bdddb113feb255328730ab0240ed1e40aadf51b
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-04 13:06:06 -0400

    Binary Tree Created in Main

--- Trees/binaryTree.cpp
+++ Trees/binaryTree.cpp
@@ -15,7 +15,14 @@ public:
 };
 
 int main() {
-    
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
 
     return 0;
 }
